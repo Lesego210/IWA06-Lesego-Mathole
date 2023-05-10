a598aa0b8ed3252790b8443dac8beb7a46400b37
Lesego210 <127973618+Lesego210@users.noreply.github.com>
2023-05-10 17:50:17 +0200

    Add files via upload

---
+++ IWA06/Challenge1.js
@@ -0,0 +1,10 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = typeof 'O74810514' === primaryPhone 
+const secondaryValid = typeof '0219131568' === secondaryPhone
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid)
